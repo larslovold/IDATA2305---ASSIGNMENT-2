6aa7ff6b1ec7ad9b9b2def788f5074a0576676c8
larsbarstad <lmlovold@stud.ntnu.no>
2025-03-03 13:11:30 +0100

    tested with volatile

--- booking/MovieTicketServer.java
+++ booking/MovieTicketServer.java
@@ -5,14 +5,14 @@ package booking;
 
 public class MovieTicketServer {
     private String movieName;
-    private int availableTickets;
+    private volatile int availableTickets;
 
     public MovieTicketServer(String movieName, int availableTickets) {
         this.movieName = movieName;
         this.availableTickets = availableTickets;
     }
 
-    public synchronized boolean bookTicket(String customerName, int requestedTickets) {
+    public boolean bookTicket(String customerName, int requestedTickets) {
         if (requestedTickets <= availableTickets) {
             System.out.println(customerName + " booked " + requestedTickets + " tickets for " + movieName);
             availableTickets -= requestedTickets;
