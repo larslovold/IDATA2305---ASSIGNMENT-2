8f2be7b6c59ae7b287ba1d3688825021dec063fe
larsbarstad <lmlovold@stud.ntnu.no>
2025-03-03 12:18:59 +0100

    Client class done

---
+++ MovieTicketClient.java
@@ -0,0 +1,18 @@
+package booking;
+
+public class MovieTicketClient extends Thread {
+    private MovieTicketServer ticketServer;
+    private String customerName;
+    private int ticketsToBook;
+
+    public MovieTicketClient(MovieTicketServer ticketServer, String customerName, int ticketsToBook) {
+        this.ticketServer = ticketServer;
+        this.customerName = customerName;
+        this.ticketsToBook = ticketsToBook;
+    }
+
+    @Override
+    public void run() {
+        ticketServer.bookTicket(customerName, ticketsToBook);
+    }
+}
